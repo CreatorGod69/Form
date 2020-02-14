0f6cf24f69d2d3dfc161a72cf7e21dc2d7b86369
Creator God69 <rainbowdeity70@gmail.com>
2020-02-14 11:08:08 +0500

    Form

---
+++ app.js
@@ -0,0 +1,21 @@
+let button = document.getElementById('login-button');
+button.addEventListener('click',function (ev) {
+    let name = document.forms['myform'].elements['login-name'].value;
+    let pass = document.forms['myform'].elements['login-name'].value;
+
+    let obj = {
+        name:name,
+        pass:pass
+    };
+    fetch('/add', {method: 'POST',body:JSON.stringify(obj),headers:{'content-type': 'application/json'}})
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                let us = document.createElement('h2');
+                us.innerHTML = data.name;
+                let mydiv = document.getElementById('tester');
+                mydiv.appendChild(us);
+            })
+            .catch(alert);
+})
